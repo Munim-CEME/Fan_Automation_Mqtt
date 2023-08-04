227cc9c858334653c64a90667edb7e80f30f08d4
Munim <monimshafi@gmail.com>
2023-08-04 22:32:56 +0500

    crashing in type casting

--- main/app_main.c
+++ main/app_main.c
@@ -11,6 +11,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <time.h>
 #include "esp_wifi.h"
 #include "esp_system.h"
 #include "nvs_flash.h"
@@ -29,9 +30,17 @@
 
 #include "esp_log.h"
 #include "mqtt_client.h"
+#include "esp_sntp.h"
 
 static const char *TAG = "MQTT_EXAMPLE";
 
+#define TAG "NTP TIME"
+
+static char mqtt_payload[50];
+
+SemaphoreHandle_t got_time_semaphore;
+bool BUSY = 0;
+void Set_Schedule(int seconds);
 
 static void log_error_if_nonzero(const char *message, int error_code)
 {
@@ -39,6 +48,9 @@ static void log_error_if_nonzero(const char *message, int error_code)
         ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
     }
 }
+
+
+
 
 /*
  * @brief Event handler registered to receive MQTT events
@@ -59,13 +71,13 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     switch ((esp_mqtt_event_id_t)event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-        msg_id = esp_mqtt_client_publish(client, "my_topic", "munim", 0, 2, 0);
+        msg_id = esp_mqtt_client_publish(client, "time_topic", "CONNECTION Established", 0, 2, 0);
         ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
 
         // msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-        ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+        // ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
-        msg_id = esp_mqtt_client_subscribe(client, "my_topic", 2);
+        msg_id = esp_mqtt_client_subscribe(client, "time_topic", 2);
         ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
 
         // msg_id = esp_mqtt_client_unsubscribe(client, "my_topic");
@@ -87,20 +99,39 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
         break;
     case MQTT_EVENT_DATA:
+       {
+        if (strncmp(event->topic, "time_topic", event->topic_len) == 0){
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
-        printf("DATA=%.*s\r\n", event->data_len, event->data);
 
-        msg_id = esp_mqtt_client_unsubscribe(client, "my_topic");
-        ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        
+
+        int seconds = atoi(event->data);
+
+        if (seconds == 0 && *event->data != '0'){
+            printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+            printf("DATA=%.*s\r\n", event->data_len, event->data);
+        }
+
+        else{
+            Set_Schedule(seconds);
+        }
+
+        }
+         else{}
+        
         break;
+       } 
+
+        // msg_id = esp_mqtt_client_unsubscribe(client, "my_topic");
+        // ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+        
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
         if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
             log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
             log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
             log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
-            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno)); 
 
         }
         break;
@@ -110,65 +141,129 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     }
 }
 
+esp_mqtt_client_handle_t client = NULL;
+
 static void mqtt_app_start(void)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
         .broker.address.uri = CONFIG_BROKER_URL,
     };
-#if CONFIG_BROKER_URL_FROM_STDIN
-    char line[128];
-
-    if (strcmp(mqtt_cfg.broker.address.uri, "FROM_STDIN") == 0) {
-        int count = 0;
-        printf("Please enter url of mqtt broker\n");
-        while (count < 128) {
-            int c = fgetc(stdin);
-            if (c == '\n') {
-                line[count] = '\0';
-                break;
-            } else if (c > 0 && c < 127) {
-                line[count] = c;
-                ++count;
-            }
-            vTaskDelay(10 / portTICK_PERIOD_MS);
-        }
-        mqtt_cfg.broker.address.uri = line;
-        printf("Broker url: %s\n", line);
-    } else {
-        ESP_LOGE(TAG, "Configuration mismatch: wrong broker url");
-        abort();
-    }
-#endif /* CONFIG_BROKER_URL_FROM_STDIN */
-
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+     client = esp_mqtt_client_init(&mqtt_cfg);
     /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
 }
 
+void print_time()
+{
+    time_t now = 0;
+    time(&now);
+    struct tm *time_info = localtime(&now);
+
+    char time_buffer[50];
+    strftime(time_buffer, sizeof(time_buffer), "%c", time_info);
+    // esp_mqtt_client_publish(client, "time_topic", time_buffer, 0, 2, 0);
+
+
+    ESP_LOGI(TAG, "************ %s ***********", time_buffer);
+}
+
+void on_got_time(struct timeval *tv)
+{
+    printf("on got callback %lld\n", tv->tv_sec);
+    print_time();
+
+    xSemaphoreGive(got_time_semaphore);
+}
+
+
+void Time_init(){
+    esp_sntp_init();
+    esp_sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+    esp_sntp_setservername(0, "pool.ntp.org");
+     esp_sntp_set_time_sync_notification_cb(on_got_time);
+
+}
+
+void Set_gpio(void *param);
+
+void Current_time(void *paramss){
+   
+
+     xSemaphoreTake(got_time_semaphore, portMAX_DELAY);
+
+    time_t now;
+    struct tm time_info;
+
+    while (1) {
+        time(&now);
+        localtime_r(&now,  &time_info);
+        printf("Current time: %lld\n", now);
+        strftime(mqtt_payload, sizeof(mqtt_payload), "%c",  &time_info);
+        esp_mqtt_client_publish(client, "time_topic", mqtt_payload, 0, 2, 0);
+
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+
+}
+
+
+
+void OnConnect(void *params);
+
+void Set_Schedule(int seconds){
+    if (BUSY)
+    {
+        esp_mqtt_client_publish(client, "time_topic", "The relay event is already scheduled", 0, 2, 0);
+    }
+    else{
+        time_t now1;
+                struct tm schedule_time;
+                struct tm current_time;
+                printf("time to be added: %s\n", seconds);
+                time(&now1);
+                
+                localtime_r(&now1,  &current_time);
+
+                // Calculate scheduled time by adding seconds
+                now1 += seconds;
+                localtime_r(&now1,  &schedule_time);
+                //mktime( &schedule_time);
+
+                char scheduled_time_str[30];
+                char message[] = " schedule time is: ";
+    
+                strftime(scheduled_time_str, sizeof(scheduled_time_str), "%c", &schedule_time);
+                printf("scheduled time is: %s\n", scheduled_time_str);
+                   strcat(message, scheduled_time_str);
+
+                // Publish the scheduled time back to a topic
+                esp_mqtt_client_publish(client, "scheduled_time_topic", message, 0, 2, 0);
+    }
+    
+}
+
+
+
 void app_main(void)
 {
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+     got_time_semaphore = xSemaphoreCreateBinary();
 
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("mqtt_client", ESP_LOG_VERBOSE);
-    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
-    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
-    esp_log_level_set("outbox", ESP_LOG_VERBOSE);
+    setenv("TZ", "AEST-10AEDT-11,M10.5.0/02:00:00,M4.1.0/03:00:00", 1);
+    tzset();
+
+    printf("first time print\n");
+    print_time();           
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
     ESP_ERROR_CHECK(example_connect());
 
+    Time_init();
     mqtt_app_start();
+    xTaskCreate(Current_time, "time_task", configMINIMAL_STACK_SIZE * 5, NULL, 5, NULL);
+
+    
 }
+//}
